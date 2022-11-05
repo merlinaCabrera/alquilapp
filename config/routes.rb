ad8dd9f931f0ba8e0f786203e0dae8ba5ba85d3c
merlinaCabrera <merlinacabreramc@gmail.com>
2022-11-05 06:25:31 +0000

    commit 0.3

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
 
 
   get '/manejo_supervisores', to: 'home#manejo_supervisores', as: 'manejo_s'
+  get '/agregar_supervisor', to: 'home#agregar_supervisor', as: 'agregar_s'
+  get '/eliminar_supervisor', to: 'home#eliminar_supervisor', as: 'eliminar_s'
+  get '/modificar_supervisor', to: 'home#modificar_supervisor', as: 'modificar_s'
   # Defines the root path route ("/")
   root "home#index"
 end
