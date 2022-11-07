9d8b16967e96fd06e78ad304b01c266678f69da9
Darriux <daribosate7@gmail.com>
2022-11-07 00:03:09 +0000

    commit 0.4

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,12 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+    protected
+
+    def after_sign_up_path_for(resource)
+        '/home/index.html.erb'
+    end
+
+    def after_sign_out_path_for(resource)
+        '/home/index.html.erb'
+    end
+
 end
