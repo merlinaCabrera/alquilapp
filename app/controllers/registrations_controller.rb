56fa6f724e4021faa61ff23b8d4c6efe63f405a8
Darriux <daribosate7@gmail.com>
2022-11-07 00:26:52 +0000

    commit 0.41

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,11 +2,11 @@ class RegistrationsController < Devise::RegistrationsController
     protected
 
     def after_sign_up_path_for(resource)
-        '/home/index.html.erb'
+        '/home/inicio.html'
     end
 
     def after_sign_out_path_for(resource)
-        '/home/index.html.erb'
+        '/devise/registrations/new.html.erb'
     end
 
 end
