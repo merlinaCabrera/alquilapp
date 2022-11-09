cc8dea081e1e8dc6049cb1909fb3e6fce7ef8177
merlinaCabrera <merlinacabreramc@gmail.com>
2022-11-09 14:51:29 +0000

    commit 0.4.5

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: "users/registrations" }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get '/manejo_supervisores', to: 'home#manejo_supervisores', as: 'manejo_s'
-  get '/agregar_supervisor', to: 'home#agregar_supervisor', as: 'agregar_s'
-  get '/eliminar_supervisor', to: 'home#eliminar_supervisor', as: 'eliminar_s'
-  get '/modificar_supervisor', to: 'home#modificar_supervisor', as: 'modificar_s'
+  get '/users/manejo_supervisores', to: 'users#manejo_supervisores', as: 'manejo_s'
+  get '/users/agregar_supervisor', to: 'users#agregar_supervisor', as: 'agregar_s'
+  get '/users/eliminar_supervisor', to: 'users#eliminar_supervisor', as: 'eliminar_s'
+  get '/users/modificar_supervisor', to: 'users#modificar_supervisor', as: 'modificar_s'
   get '/billetera', to: 'home#billetera', as: 'bille'
   
   # Defines the root path route ("/")
