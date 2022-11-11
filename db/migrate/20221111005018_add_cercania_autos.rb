ed3900db318c1381731174855bba4cf8c5da3992
merlinaCabrera <113050277+merlinaCabrera@users.noreply.github.com>
2022-11-11 17:45:30 -0300

    Revert "commit 0.6.4"

--- db/migrate/20221111005018_add_cercania_autos.rb
+++
@@ -1,6 +0,0 @@
-class AddCercaniaAutos < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :vehiculos, :cercania
-    add_column :vehiculos, :cercano, :integer, default: 0
-  end
-end
--- db/migrate/20221111025749_add_ocupado.rb
+++
@@ -1,5 +0,0 @@
-class AddOcupado < ActiveRecord::Migration[7.0]
-  def change
-    add_column :vehiculos, :ocupado, :boolean, default: false
-  end
-end
