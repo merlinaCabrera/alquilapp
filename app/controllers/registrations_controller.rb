a204b6e0a65be8db89219d6d1c93d861e9690ac9
ezebaez <baezeze01@gmailcom>
2022-11-28 22:16:13 -0300

    6.9.1

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,11 +1,6 @@
 class RegistrationsController < Devise::RegistrationsController
   protected
 
-  def after_sign_up_path_for(resource)
-    '/home/index.html.erb'
-  end
+  
 
-  def after_sign_out_path_for(resource)
-    '/home/index.html.erb'
-  end
 end
