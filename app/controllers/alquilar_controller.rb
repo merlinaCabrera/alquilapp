f42f40f73b8fcd28a683f3931d32ad053e2f9520
Darriux <daribosate7@gmail.com>
2022-12-11 16:33:17 -0300

    commit 0.7.4

--- app/controllers/alquilar_controller.rb
+++ app/controllers/alquilar_controller.rb
@@ -6,7 +6,6 @@ class AlquilarController < ApplicationController
   def create
     @alquilar = Alquilar.new
     @alquilar.precio_alquiler = (params[:precio_alquiler])
-    current_user.update(balance: (current_user.balance - @alquilar.precio_alquiler))
     @alquilar.id_vehiculo = params[:id_vehiculo]
     @alquilar.id_user = params[:id_user]
     @alquilar.inicio = params[:inicio]
@@ -22,6 +21,7 @@ class AlquilarController < ApplicationController
   def destroy
     @alquilar = Alquilar.where(id_user: current_user.id).find_by_activo(true)
     @alquilar.activo = false
+    current_user.update(balance: (current_user.balance - @alquilar.precio_alquiler))
     @alquilar.save
     redirect_to root_path, notice: 'Alquiler cancelado', status: :see_other
   end
